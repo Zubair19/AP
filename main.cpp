b992ea6a6fcbbcae69e57e9213dcaa754c54ac8e
Zubair19 <11besezaslam@seecs.edu.pk>
2015-03-03 00:37:53 +0500

    lsb2

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include "mycode.h"
+using namespace std;
+
+
+
+int main()
+{
+matrix m1;
+m1.set();
+m1.add();
+m1.mul();
+m1.mul_add();
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
